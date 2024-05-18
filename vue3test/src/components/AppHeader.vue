20fca38b44a5fc75e9f5cd7e251abd7b7c74d1ae
is-xiaohu-sigh <1137247090@qq.com>
2024-05-18 18:34:17 +0800

    第一次提交

---
+++ vue3test/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-container style="margin:0;padding:0">
+        <el-header style="margin:0;padding:0">
+            <div id="title">Vue 学习笔记</div>
+        </el-header>
+        <el-main style="margin: 0;padding: 0;">
+            <el-menu 
+                mode="horizontal"
+                background-color="#e8e7e3"
+                text-color="#777777"
+                active-text-color="#000"
+                :default-active="0">
+            </el-menu>
+        </el-main>
+    </el-container>
+</template>
--- vue3test/src/main.js
+++ vue3test/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { ElementPlus } from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const app = createApp(App)
+//遍历ElemetPlusIconsVue中的所有组件进行注册
+for (const [key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
+}
+app.use(ElementPlus)
+app.mount('#app')

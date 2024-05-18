f34b41cdf446579583b422801863789dd6cf76dd
is-xiaohu-sigh <1137247090@qq.com>
2024-05-18 19:27:48 +0800

    vue学习笔记

--- vue3test/src/main.js
+++ vue3test/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { ElementPlus } from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 引入图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
-//遍历ElemetPlusIconsVue中的所有组件进行注册
-for (const [key,component] of Object.entries(ElementPlusIconsVue)){
-    app.component(key,component)
+// 遍历ElementPlusIconsVue中的所有组件进行祖册
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    // 向应用实例中全局注册图标组件
+    app.component(key, component)
 }
 app.use(ElementPlus)
 app.mount('#app')
